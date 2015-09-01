0e0a10b477844f4e0c1d58653e9bf971bbdb48cd
Thiago Maneschy <thiago@maneschy.com>
2015-08-31 22:26:13 -0400

    vaiiiiii

--- admin2/views/admin/opiniao_adc.php
+++ admin2/views/admin/opiniao_adc.php
@@ -2,10 +2,15 @@
     <div class="row">
         <div class="col-md-12">
             <section class="panel panel-default">
-                <div class="panel-heading"><strong><span class="glyphicon glyphicon-th"></span> CADASTRO DE BIOGRAFIA</strong></div>
+                <div class="panel-heading"><strong><span class="glyphicon glyphicon-th"></span> CADASTRO OPINIÃO</strong></div>
                 <div class="panel-body">
                     <form role="form">
                     
+                    	<div class="col-sm-10">
+                    		<label for="exampleInputEmail1">Título</label>
+                    	    <input tooltip="Tooltip on top" type="text" class="form-control" data-toggle="tooltip" data-placement="top" tooltip-trigger="focus">
+                    	</div>
+                    	
                         <div class="form-group">
                             <label for="exampleInputEmail1">Texto</label>
                             <textarea name="" id="" class="form-control" rows="7"></textarea>

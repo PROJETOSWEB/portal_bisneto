eb8701e2e423d9a5fdf631c96000837143549afc
Thiago Maneschy <thiago@maneschy.com>
2015-09-14 22:42:13 -0400

    mexe-mexe

--- admin/noticias.php
+++ admin/noticias.php
@@ -5,6 +5,8 @@
 
 
 <link rel="stylesheet" type="text/css" href="assets/bootstrap-fileupload/bootstrap-fileupload.css" />
+<link rel="stylesheet" type="text/css" href="assets/bootstrap-datepicker/css/datepicker.css" />
+    
 <!--main content start-->
 <section id="main-content">
     <section class="wrapper">
@@ -14,10 +16,10 @@
                 <div class="panel-heading"><strong><span class="glyphicon glyphicon-th"></span> CADASTRO DE NOTÍCIA</strong></div>
                 <div class="panel-body">
                     <form role="form">
+                    
                         <div class="form-group">
-
+                        	<label for="exampleInputEmail1">SELECIONE A DATA</label>
                             <input class="form-control form-control-inline input-medium default-date-picker"  size="16" type="text" value="" />
-                            <span class="help-block">Select date</span>
                         </div>
 
                         <div class="form-group">
@@ -74,13 +76,27 @@
                             </div>
 
                             <div class="col-sm-12">
+                            	
+                            	<hr/>
+                            	
+                            	<label style="margin-right: 20px;" for="exampleInputEmail1">DESTAQUE • BANNER</label>
+                                <div class="switch switch-square"
+                                 data-on-label="<i class=' fa fa-check'></i>"
+                                	data-off-label="<i class='fa fa-times'></i>">
+                                    <input type="checkbox" />
+                                </div>
 
                                 <hr/>
+                                
                                 <label style="margin-right: 20px;" for="exampleInputEmail1">PUBLICAR?</label>
-                                <label class="switch switch-success">
-                                    <input type="checkbox" checked><i></i>
-                                </label>
-                                   <hr/>
+                               <div class="switch switch-square"
+                               	  data-off-label="<i class='fa fa-times'></i>"
+                                    data-on-label="<i class=' fa fa-check'></i>">
+                                   <input type="checkbox"  checked />
+                               </div>
+                                
+                                
+                                <hr/>
 
                             </div>
                         </div>
@@ -104,52 +120,33 @@
 
 </section>
 
-<!-- js placed at the end of the document so the pages load faster -->
-<script src="js/jquery.js"></script>
-<script src="js/jquery-1.8.3.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script class="include" type="text/javascript" src="js/jquery.dcjqaccordion.2.7.js"></script>
-<script src="js/jquery.scrollTo.min.js"></script>
-<script src="js/jquery.nicescroll.js" type="text/javascript"></script>
-<script src="js/jquery.sparkline.js" type="text/javascript"></script>
-<script src="assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js"></script>
-<script src="js/owl.carousel.js" ></script>
-<script src="js/jquery.customSelect.min.js" ></script>
-<script src="js/respond.min.js" ></script>
-<script type="text/javascript" src="assets/bootstrap-fileupload/bootstrap-fileupload.js"></script>
+    <!-- js placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script class="include" type="text/javascript" src="js/jquery.dcjqaccordion.2.7.js"></script>
+    <script src="js/jquery.scrollTo.min.js"></script>
+    <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
+    <script src="js/respond.min.js" ></script>
+  
+    <!--custom switch-->
+    <script src="js/bootstrap-switch.js"></script>
+    <!--custom tagsinput-->
+    <script src="js/jquery.tagsinput.js"></script>
+    
+    <!--custom checkbox & radio-->
+    <script type="text/javascript" src="js/ga.js"></script>
+    <!--this page plugins-->
 
+  <script type="text/javascript" src="assets/fuelux/js/spinner.min.js"></script>
+  <script type="text/javascript" src="assets/bootstrap-fileupload/bootstrap-fileupload.js"></script>
+  <script type="text/javascript" src="assets/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+  <script type="text/javascript" src="assets/ckeditor/ckeditor.js"></script>
 
+  <!--common script for all pages-->
+    <script src="js/common-scripts.js"></script>
+    <!--this page  script only-->
+    <script src="js/advanced-form-components.js"></script>
 
-<!--common script for all pages-->
-<script src="js/common-scripts.js"></script>
-
-<!--script for this page-->
-<script src="js/sparkline-chart.js"></script>
-<script src="js/easy-pie-chart.js"></script>
-<script src="js/count.js"></script>
-
-<script>
-
-    //owl carousel
-
-    $(document).ready(function () {
-        $("#owl-demo").owlCarousel({
-            navigation: true,
-            slideSpeed: 300,
-            paginationSpeed: 400,
-            singleItem: true,
-            autoPlay: true
-
-        });
-    });
-
-    //custom select box
-
-    $(function () {
-        $('select.styled').customSelect();
-    });
-
-</script>
 
 </body>
 </html>
--- admin/opiniao.php
+++ admin/opiniao.php
@@ -4,7 +4,9 @@
 <!--sidebar end-->
 
 
-<link rel="stylesheet" type="text/css" href="assets/bootstrap-fileupload/bootstrap-fileupload.css" />
+    <link rel="stylesheet" type="text/css" href="assets/bootstrap-fileupload/bootstrap-fileupload.css" />
+        <link rel="stylesheet" type="text/css" href="assets/bootstrap-datepicker/css/datepicker.css" />
+
 <!--main content start-->
 <section id="main-content">
     <section class="wrapper">
@@ -37,13 +39,11 @@
                     
                     <form role="form" action="php/salva_opiniao.php" method="POST" enctype='multipart/form-data'>
                         
-                        <div class="form-group">
-
-                            <input name="data" class="form-control form-control-inline input-medium default-date-picker"  size="16" type="text" value="" />
-                            <span class="help-block">Select date</span>
-                            
+						<div class="form-group">
+							<label for="exampleInputEmail1">SELECIONE A DATA</label>
+                            <input class="form-control form-control-inline input-medium default-date-picker"  size="16" type="text" value="" />
                         </div>
-
+						
                         <div class="form-group">
                             <label for="exampleInputEmail1">TÍTULO</label>
                             <input tooltip="Título do seu artigo" name="titulo" type="text" class="form-control" data-placement="top">
@@ -86,11 +86,11 @@
                                             <div class="fileupload-preview fileupload-exists thumbnail" style="max-width: 200px; max-height: 150px; line-height: 20px;"></div>
                                             <div>
                                                 <span class="btn btn-white btn-file">
-                                                    <span class="fileupload-new"><i class="fa fa-paper-clip"></i> Select image</span>
-                                                    <span class="fileupload-exists"><i class="fa fa-undo"></i> Change</span>
+                                                    <span class="fileupload-new"><i class="fa fa-paper-clip"></i> selecionar imagem</span>
+                                                    <span class="fileupload-exists"><i class="fa fa-undo"></i> trocar</span>
                                                     <input type="file" name="img" class="default" />
                                                 </span>
-                                                <a href="#" class="btn btn-danger fileupload-exists" data-dismiss="fileupload"><i class="fa fa-trash"></i> Remove</a>
+                                                <a href="#" class="btn btn-danger fileupload-exists" data-dismiss="fileupload"><i class="fa fa-trash"></i> excluir</a>
                                             </div>
                                         </div>
                                     </div>
@@ -101,10 +101,12 @@
 
                                 <hr/>
                                 <label style="margin-right: 20px;" for="exampleInputEmail1">PUBLICAR?</label>
-                                <label class="switch switch-success">
-                                    <input name="publicar" type="checkbox" checked value="1"><i></i>
-                                </label>
-                                   <hr/>
+                                <div class="switch switch-square"
+                                	data-off-label="<i class='fa fa-times'></i>"
+                                     data-on-label="<i class=' fa fa-check'></i>">
+                                    <input type="checkbox" checked="" value="1" />
+                                </div>
+                                <hr/>
 
                             </div>
                         </div>
@@ -128,52 +130,32 @@
 
 </section>
 
-<!-- js placed at the end of the document so the pages load faster -->
-<script src="js/jquery.js"></script>
-<script src="js/jquery-1.8.3.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script class="include" type="text/javascript" src="js/jquery.dcjqaccordion.2.7.js"></script>
-<script src="js/jquery.scrollTo.min.js"></script>
-<script src="js/jquery.nicescroll.js" type="text/javascript"></script>
-<script src="js/jquery.sparkline.js" type="text/javascript"></script>
-<script src="assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js"></script>
-<script src="js/owl.carousel.js" ></script>
-<script src="js/jquery.customSelect.min.js" ></script>
-<script src="js/respond.min.js" ></script>
-<script type="text/javascript" src="assets/bootstrap-fileupload/bootstrap-fileupload.js"></script>
+    <!-- js placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script class="include" type="text/javascript" src="js/jquery.dcjqaccordion.2.7.js"></script>
+    <script src="js/jquery.scrollTo.min.js"></script>
+    <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
+    <script src="js/respond.min.js" ></script>
+  <!--custom switch-->
+  <script src="js/bootstrap-switch.js"></script>
+  <!--custom tagsinput-->
+  <script src="js/jquery.tagsinput.js"></script>
+  <!--this page plugins-->
+  
+    <!--this page plugins-->
+
+  <script type="text/javascript" src="assets/fuelux/js/spinner.min.js"></script>
+  <script type="text/javascript" src="assets/bootstrap-fileupload/bootstrap-fileupload.js"></script>
+  <script type="text/javascript" src="assets/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+  <script type="text/javascript" src="assets/ckeditor/ckeditor.js"></script>
+
+  <!--common script for all pages-->
+    <script src="js/common-scripts.js"></script>
+    <!--this page  script only-->
+    <script src="js/advanced-form-components.js"></script>
 
 
-
-<!--common script for all pages-->
-<script src="js/common-scripts.js"></script>
-
-<!--script for this page-->
-<script src="js/sparkline-chart.js"></script>
-<script src="js/easy-pie-chart.js"></script>
-<script src="js/count.js"></script>
-
-<script>
-
-    //owl carousel
-
-    $(document).ready(function () {
-        $("#owl-demo").owlCarousel({
-            navigation: true,
-            slideSpeed: 300,
-            paginationSpeed: 400,
-            singleItem: true,
-            autoPlay: true
-
-        });
-    });
-
-    //custom select box
-
-    $(function () {
-        $('select.styled').customSelect();
-    });
-
-</script>
 
 </body>
 </html>
